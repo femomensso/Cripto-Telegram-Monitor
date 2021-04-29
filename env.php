d4d132443e2fa3dc0f8d47f747d3763bd3094cd2
Fernando Momensso <femomensso@gmail.com>
2021-04-28 21:07:43 -0300

    Delete env.php

--- env.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$_ENV['msg_time'] = 20.0;
-$_ENV['coins'] = ['BTC', 'ETH', 'LTC', 'XRP'];
-$_ENV['telegram_bot'] = '1779101149:AAGJNqzFRWSl6piFkmMMoFSJb9SAq_wxH-A';
-$_ENV['chat_id'] = '-454818039';
-
-?>
